6d7891ba8380f1b4c89e3b9e7bd8aa64db7f6af4
Your Name <ericyucheng.zhang@mail.utoronto.ca>
2024-11-15 19:23:51 -0500

    eric's work

--- src/main/java/TranscriptionReader.java
+++ src/main/java/TranscriptionReader.java
@@ -1,4 +1,21 @@
-package PACKAGE_NAME;
+package use_case;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
-public class TranscriptionReader {
-}
+    /**
+     * Utility class responsible for reading the transcription from a file.
+     */
+    public class TranscriptionReader {
+        /**
+         * Reads the transcription from the given file path.
+         *
+         * @param filePath Path to the transcription file.
+         * @return Transcription text.
+         * @throws IOException If an I/O error occurs.
+         */
+        public static String readTranscription(String filePath) throws IOException {
+            return new String(Files.readAllBytes(Paths.get(filePath)));
+        }
+    }
+
