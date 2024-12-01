b0779b48b9ceaa6a3fb7042459917f97f21df830
Your Name <ericyucheng.zhang@mail.utoronto.ca>
2024-11-30 21:48:01 -0500

    23

--- src/main/java/use_case/summary_to_quiz/SummaryToQuizOutputBoundary.java
+++ src/main/java/use_case/summary_to_quiz/SummaryToQuizOutputBoundary.java
@@ -1,18 +1,24 @@
 package use_case.summary_to_quiz;
 
 /**
- * The output boundary for the Login Use Case.
+ * The output boundary for the SummaryToQuiz Case.
  */
-public interface QuizServiceOutputBoundary {
-        /**
-         * Prepares the success view for the Login Use Case.
-         * @param outputData the output data
-         */
-        void prepareSuccessView(QuizServiceOutputData outputData);
+public interface SummaryToQuizOutputBoundary {
+    /**
+     * Prepares the success view for the SummaryToQuiz Use Case.
+     *
+     * @param outputData the output data
+     */
+    static void prepareSuccessView(SummaryToQuizOutputData outputData) {
 
-        /**
-         * Prepares the failure view for the Login Use Case.
-         * @param errorMessage the explanation of the failure
-         */
-        void prepareFailView(String errorMessage);
     }
+
+    /**
+     * Prepares the failure view for the SummaryToQuiz Use Case.
+     *
+     * @param errorMessage the explanation of the failure
+     */
+    static void prepareFailView(String errorMessage) {
+        
+    }
+}
