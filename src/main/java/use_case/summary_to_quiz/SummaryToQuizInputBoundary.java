106b57177c219d4d966d549dc100f7cde800a5ee
Your Name <ericyucheng.zhang@mail.utoronto.ca>
2024-11-30 21:50:22 -0500

    28

--- src/main/java/use_case/summary_to_quiz/SummaryToQuizInputBoundary.java
+++ src/main/java/use_case/summary_to_quiz/SummaryToQuizInputBoundary.java
@@ -1,4 +1,13 @@
 package use_case.summary_to_quiz;
 
-public class SummaryToQuizInputBoundary {
+/**
+ * Input Boundary for actions which are related to summary to quiz.
+ */
+
+public interface SummaryToQuizInputBoundary {
+    /**
+     * Executes the SummaryToQuiz use case.
+     * @param summaryToQuizInputData the input data
+     */
+    void execute(SummaryToQuizInputData summaryToQuizInputData);
 }
