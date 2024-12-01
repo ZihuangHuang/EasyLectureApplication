5fb9428a90120d9efc7fbf223a8b1a8736bb4dfd
Your Name <ericyucheng.zhang@mail.utoronto.ca>
2024-11-30 21:47:19 -0500

    21

--- src/main/java/entity/Quiz.java
+++ src/main/java/entity/Quiz.java
@@ -1,4 +1,41 @@
 package entity;
 
+import java.util.List;
+
 public class Quiz {
+    private String content;
+    private List<String> choices;
+    private String answer;
+
+    public Quiz(String content, List<String> choices, String answer) {
+        this.content = content;
+        this.choices = choices;
+        this.answer = answer;
+    }
+
+    public String getcontent() {
+        return content;
+    }
+
+    public List<String> getchoices() {
+        return choices;
+    }
+
+    public String getanswer() {
+        return answer;
+    }
+
+    public void setcontent(String content) {
+        this.content = content;
+    }
+
+    public void setchoices(List<String> choices) {
+        this.choices = choices;
+    }
+
+    public void setanswer(String answer) {
+        this.answer = answer;
+    }
+
+
 }
