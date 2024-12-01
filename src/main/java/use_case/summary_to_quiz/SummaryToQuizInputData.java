10296b676fb2eaa86e01152815f7fdb92e97c410
Your Name <ericyucheng.zhang@mail.utoronto.ca>
2024-11-30 21:49:16 -0500

    25

--- src/main/java/use_case/summary_to_quiz/SummaryToQuizInputData.java
+++ src/main/java/use_case/summary_to_quiz/SummaryToQuizInputData.java
@@ -3,14 +3,14 @@ package use_case.summary_to_quiz;
 /**
  * The input data for the commonQuizFactory Use Case.
  */
-public class QuizServiceInputData {
-    private final String transcript;
+public class SummaryToQuizInputData {
+    private static String summary = "";
 
-    public QuizServiceInputData(String transcript) {
-        this.transcript = transcript;
+    public SummaryToQuizInputData(String summary) {
+        this.summary = summary;
     }
 
-    String getTranscript() {
-        return transcript;
+    static String getSummary() {
+        return summary;
     }
 }
