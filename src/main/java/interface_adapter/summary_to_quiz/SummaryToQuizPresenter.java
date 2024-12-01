d6d8950cc7708c55d77bb558afde4129a2879df9
Your Name <ericyucheng.zhang@mail.utoronto.ca>
2024-11-30 23:15:18 -0500

    eric 4

--- src/main/java/interface_adapter/summary_to_quiz/SummaryToQuizPresenter.java
+++ src/main/java/interface_adapter/summary_to_quiz/SummaryToQuizPresenter.java
@@ -1,7 +1,7 @@
-package Interface_adapter.summary_to_quiz;
+package interface_adapter.summary_to_quiz;
 
 import use_case.summary_to_quiz.SummaryToQuizOutputBoundary;
-    import use_case.summary_to_quiz.SummaryToQuizOutputData;
+import use_case.summary_to_quiz.SummaryToQuizOutputData;
 /**
  * Presenter class that formats and presents the quizzes.
  */
