b4ba80d30c1c2840c2e58b2c641488e83e1f7023
Your Name <ericyucheng.zhang@mail.utoronto.ca>
2024-11-30 21:45:58 -0500

    18

--- src/main/java/Interface_adapter/SummaryToQuizViewModel.java
+++ src/main/java/Interface_adapter/SummaryToQuizViewModel.java
@@ -1,13 +1,34 @@
 package Interface_adapter;
 
+import entity.Quiz;
+import use_case.summary_to_quiz.SummaryToQuiz;
+
+import java.util.List;
+
 /**
  * The View Model for the Login View.
  */
-public class SummaryToQuizViewModel extends ViewModel<LoginState> {
+public class SummaryToQuizViewModel {
+    private Quiz quiz;
 
-    public LoginViewModel() {
-        super("log in");
-        setState(new LoginState());
+    public SummaryToQuizViewModel() {
+        SummaryToQuiz summaryToQuiz = new SummaryToQuiz();
+    }
+
+    public void getQuiz() {
+        quiz.getcontent();
+    }
+
+    public void getChoices() {
+         quiz.getchoices();
+    }
+
+    public void setQuiz(String getcontent) {
+        quiz.setcontent(getcontent);
+    }
+
+    public void setChoices(List<String> getchoices) {
+        quiz.setchoices(getchoices);
     }
 }
 
